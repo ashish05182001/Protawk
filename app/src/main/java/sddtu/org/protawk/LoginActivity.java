de017e200c12a4d67eed1609747cded4fb002c24
batbrain7 <kumar.mohit983@gmail.com>
2017-04-08 22:22:13 +0530

    Sign Up made

--- app/src/main/java/sddtu/org/protawk/LoginActivity.java
+++ app/src/main/java/sddtu/org/protawk/LoginActivity.java
@@ -158,7 +158,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             @Override
             public void onClick(View v) {
 
-                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                Intent intent=new Intent(LoginActivity.this,SignUp.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/sddtu/org/protawk/SignUp.java
@@ -0,0 +1,19 @@
+package sddtu.org.protawk;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        getSupportActionBar().setTitle("Register Here");
+        
+    }
+}
