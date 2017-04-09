022ad8fc745373010fcf284e3fb8c26c2ee38387
batbrain7 <kumar.mohit983@gmail.com>
2017-04-09 20:19:54 +0530

    Profile page

--- app/src/main/java/sddtu/org/protawk/SignUp.java
+++ app/src/main/java/sddtu/org/protawk/SignUp.java
@@ -1,11 +1,15 @@
 package sddtu.org.protawk;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,10 @@ public class SignUp extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
 
         getSupportActionBar().setTitle("Register Here");
+        button = (Button) findViewById(R.id.signup_button);
+    }
 
+    public void SignUp(View view) {
+        startActivity(new Intent(getApplicationContext(),SPprrofile.class));
     }
 }
